e4affb07a9696d99f4cb90baefed40bcb4115e63
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-23 12:16:12 +0600

    add number.js with variable declarations and type logging

---
+++ number.js
@@ -0,0 +1,15 @@
+let age = 28; //Integer
+
+console.log(age)
+
+let price = 99.99;
+console.log(price)
+
+let negative = -5;
+
+console.log(negative)
+
+let bigNumber = 1_000_000;
+console.log(bigNumber)
+
+console.log(typeof age)
