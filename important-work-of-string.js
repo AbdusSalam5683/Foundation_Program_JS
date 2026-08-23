9aac5f62cce4fd70fffdb1b89318f5ae79a6be49
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-23 12:09:44 +0600

    add additional string manipulation examples in important-work-of-string.js

--- important-work-of-string.js
+++ important-work-of-string.js
@@ -2,3 +2,8 @@ let str = "JavaScript"
 console.log(str.length)
 console.log(str[0])
 
+console.log(str.toUpperCase())
+console.log(str.toLocaleLowerCase())
+console.log(str.slice(0, 3))
+console.log(str.slice(0, 3).toUpperCase())
+console.log(str.includes("Script"))
