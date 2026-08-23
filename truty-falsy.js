b3779cea502c480a22063c31ece7872aac3a3e98
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-23 13:00:55 +0600

    add truty-falsy.js with examples of falsy and truthy values

---
+++ truty-falsy.js
@@ -0,0 +1,13 @@
+// Falsy  false :
+Boolean(0)         // false
+Boolean("")        // false
+Boolean(null)      // false
+Boolean(undefined) // false
+Boolean(NaN)       // false
+Boolean(false)     // false
+
+// Truthy  true :
+Boolean(1)         // true
+Boolean("Hello")   // true
+Boolean([])        // true
+Boolean({})        // true
