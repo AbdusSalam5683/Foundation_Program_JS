e7230f2f08d8055e30a2e5b1f19d1ac4632c9573
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-24 13:09:41 +0600

    add examples of BigInt usage with correct results

--- biglnt.js
+++ biglnt.js
@@ -13,3 +13,7 @@ console.log(maxNumber + 1);
 
 console.log(maxNumber + 2);
 console.log(BigInt(maxNumber + 2));
+
+// ৩. BigInt ব্যবহার করলে:
+console.log(9007199254740991n + 1n); // 9007199254740992n
+console.log(9007199254740991n + 2n); // 9007199254740993n (সঠিক!)
