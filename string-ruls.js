b70906f1f6fc1b770ab326e109c49e34b4ba4935
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-24 19:22:45 +0600

    add string manipulation examples with template literals

---
+++ string-ruls.js
@@ -0,0 +1,10 @@
+let firstName = "Shafayat"
+let lastName ="Islam"
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+let fullName2 = `${firstName} ${lastName} \nNaoga`
+
+
+console.log(fullName2)
