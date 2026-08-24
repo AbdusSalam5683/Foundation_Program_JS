c46a4923537b9e27269c9c5220c061a9c987fb60
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-24 18:23:40 +0600

    add initial array declarations and console logs

---
+++ array.js
@@ -0,0 +1,8 @@
+let fruits =["Apple", "Banana", "Mango"];
+let numbers = [10, 20, 30, 40];
+let mixed =["Salam", 28, true, null];
+
+console.log(fruits, numbers, mixed);
+
+
+console.log(Array.isArray(fruits))
