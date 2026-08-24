c48ded06624e9cb396666420c038dee4ae3fac46
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-08-24 20:02:57 +0600

    add grading logic based on marks with conditional statements

--- condisonal.js
+++ condisonal.js
@@ -8,3 +8,19 @@ if(age >= 18){
 }
 
 
+
+let marks = 33;
+
+if(marks>=80){
+    console.log("A+")
+}else if(marks >=70){
+    console.log("A")
+}else if(marks >= 60){
+    console.log("-A")
+}else if(marks >= 50){
+    console.log("C")
+}else if(marks >=33){
+    console.log("D")
+}else{
+    console.log("F")
+}
